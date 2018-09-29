c14546b79ed1d8236acaa1720dedd4c46cb09e2c
pray-lee <praycis@gmail.com>
2018-09-29 17:05:10 +0800

    add srver

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('koa server-hello world')
+const Koa = require('koa')
+const app = new Koa()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`server is running at port ${port}......`);
+})
