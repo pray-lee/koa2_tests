cd3ba1d7d8684e6b82a6641aca6279490c7278ab
pray-lee <praycis@gmail.com>
2018-09-29 17:21:59 +0800

    update

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const Koa = require('koa')
 const app = new Koa()
 const port = process.env.PORT || 3000
 
+//base
+app.use(async(ctx, next) => {
+  await next()
+  ctx.response.type = 'text/html'
+  ctx.response.body = '<h1>hello world</hi>'
+})
+
 app.listen(port, () => {
   console.log(`server is running at port ${port}......`);
 })
