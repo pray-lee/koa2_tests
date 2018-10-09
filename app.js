a5b00753b54318746b36c15cfd380922ab95fd99
pray-lee <praycis@gmail.com>
2018-10-09 15:55:18 +0800

    update

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const Koa = require('koa')
 const app = new Koa()
 const port = process.env.PORT || 3000
 
-//base
-app.use(async(ctx, next) => {
+
+//use middleware
+app.use(async (ctx, next) => {
   await next()
   ctx.response.type = 'text/html'
-  ctx.response.body = '<h1>hello world</hi>'
+  ctx.response.body = '<h1 style="color:#ff5252">hello koa!</h1>'
 })
-
 app.listen(port, () => {
-  console.log(`server is running at port ${port}......`);
+  `server is running at port ${port}`
 })
